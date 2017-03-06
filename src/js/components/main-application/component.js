54220b3769b8059f594d25812299340d6d538bb5
Anssi Herranen <anssi.herranen@gmail.com>
2017-03-06 17:55:30 +0200

    Fixed bug with counting renderable symbols

--- src/js/components/main-application/component.js
+++ src/js/components/main-application/component.js
@@ -6,7 +6,7 @@ const HEART_SYMBOL =  '\u2665'
 const MainApplication = (props) => (
   <h1>
     <span onClick={props.handleClick}>ClickMe</span>
-    {Array(props.heartCounter).join(HEART_SYMBOL)}
+    {Array(props.heartCounter + 1).join(HEART_SYMBOL)}
   </h1>
 )
 
